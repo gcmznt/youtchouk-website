41b198932ad60c15d4cc591f3c0cb45fef6e7177
Giacomo Zinetti <giacomo.zinetti@giko.it>
2012-02-06 00:06:31 +0100

    initial commit

---
+++ www/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>YouTchouk.com ^_^</title>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
+    
+    <link rel="stylesheet" type="text/css" href="resources/css/reset-min.css" />
+    <link href="http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css" />
+    
+    <script type="text/javascript" src="resources/js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="resources/js/jquery.tools.min.js"></script>
+    <script type="text/javascript" src="resources/js/javascript.js"></script>
+    <script type="text/javascript">
+        <?php if (isset($_GET['q'])) { ?>
+        var main_feed = 'https://gdata.youtube.com/feeds/api/users/tchoukballpromotion/uploads?alt=json&orderby=updated&max-results=30&q=<?php echo $_GET['q']; ?>';
+        <?php } else { ?>
+        var main_feed = 'https://gdata.youtube.com/feeds/api/users/tchoukballpromotion/uploads?alt=json&orderby=updated&max-results=30';
+        <?php } ?>
+    </script>
+</head>
+<body>
+    <div id="wrapper">
+        <h1><a href="./">YouTchouk</a></h1>
+        <div id="menu" class="box">
+            <ul>
+                <li><a href="?q=ewc">EWC</a></li>
+                <li><a href="#">Categoria 2</a></li>
+                <li><a href="#">Categoria 3</a></li>
+                <li><a href="#">Categoria 4</a></li>
+                <li><a href="#">Categoria 5</a></li>
+                <li><a href="#">Categoria 6</a></li>
+                <li><a href="#">Categoria 7</a></li>
+                <li><a href="#">Categoria 8</a></li>
+            </ul>
+        </div>
+        <?php if (isset($_GET['v'])) { ?>
+        <div id="video" class="box">
+            <iframe width="640" height="480" src="http://www.youtube.com/embed/<?php echo $_GET['v']; ?>" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <?php } else { ?>
+        <div id="titolo" class="box">
+            <h2><?php
+                if (isset($_GET['q']))
+                    echo 'Search results for '.$_GET['q'];
+                else
+                    echo 'Most recent';
+            ?></h2>
+        </div>
+        <div id="results" class="box">
+            <div id="search_result">
+                <div class="items">
+
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+        <div id="search" class="box">
+            <form id="search_form">
+                <input id="search_text" />
+            </form>
+        </div>
+        <div id="most_viewed" class="box">
+            <h3>Most viewed</h3>
+            <ul></ul>
+        </div>
+        <div id="top" class="box">
+            <h3>Best</h3>
+            <ul></ul>
+        </div>
+        <div id="adsense" class="box">
+            <div class="adsense_wrapper">
+                <script type="text/javascript">
+                <!--
+                    google_ad_client = "pub-9138021364556759";
+                    /* 468x60, creato 18/10/08 */
+                    google_ad_slot = "4149347112";
+                    google_ad_width = 468;
+                    google_ad_height = 60;
+                -->
+                </script>
+                <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>        </div>
+            </div>
+        <div id="social" class="box">
+            <a id="fb" href="http://www.facebook.com/pages/YouTchouk/140981555717"></a>
+            <a id="yt" href="http://www.youtube.com/youtchouk"></a>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-6536789-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+</body>
+</html>
+    
---
+++ www/resources/js/javascript.js
@@ -0,0 +1,72 @@
+
+    $(document).ready(function() {
+
+        $("#search_result").append('<div class="navi"></div>');
+        $("#search_result").scrollable();
+        loadFeed(main_feed, '#search_result .items', 'full', 6);
+        loadFeed('https://gdata.youtube.com/feeds/api/users/tchoukballpromotion/uploads?alt=json&orderby=viewCount&max-results=6', '#most_viewed ul', 'list_views');
+        loadFeed('https://gdata.youtube.com/feeds/api/users/tchoukballpromotion/uploads?alt=json&orderby=rating&max-results=6', '#top ul', 'list');
+
+        $('#search_form').submit(function() {
+            var query = $('#search_text').val();
+            document.location.href = '?q='+query;
+            return false;
+        });
+    });
+
+    function loadFeed(feed, domId, style, videoPerPage) {
+        videoPerPage = videoPerPage || 0;
+
+        if($(domId).length === 0)
+            return;
+
+        var container = $(domId);
+
+        $.ajax({
+            url: feed,
+            dataType: 'jsonp',
+            success: function(data) {
+                if (videoPerPage > 0) {
+                    var pageTpl = $('<div class="page"></div>');
+                    var page = pageTpl.clone();
+                    for(i = 1, l = data.feed.entry.length; i < l + 1; i++) {
+                        page.append(_formatVideo(data.feed.entry[i - 1], style));
+                        if((i % videoPerPage) === 0) {
+                            container.append(page);
+                            page = pageTpl.clone();
+                        }
+                    }
+                    if (page.html() != '')
+                        container.append(page);
+                } else {
+                    for(i = 1, l = data.feed.entry.length; i < l + 1; i++) {
+                        container.append(_formatVideo(data.feed.entry[i - 1], style));
+                    }
+                }
+                $("#search_result").navigator();
+            },
+            error: function(wtf) {/*console.log(wtf);*/}
+        });
+    }
+
+    function _formatVideo(video, style) {
+        id = video.id.$t.substr(video.id.$t.lastIndexOf('/')+1);
+        thumb = video.media$group.media$thumbnail[0].url;
+        views = video.yt$statistics.viewCount;
+        title = video.title.$t;
+        rate = (video.gd$rating) ? video.gd$rating.average : 0;
+
+        if (style == 'full')
+            return '<div class="video"><a href="/?v=' + id + '"><img src="' + thumb + '" /></a><p>' + title + '</p></div>';
+        if (style == 'list')
+            return '<li><a href="/?v=' + id + '">' + title + '</a></li>';
+        if (style == 'list_views')
+            return '<li><a href="/?v=' + id + '">'+views+' &bull; ' + title + '</a></li>';
+        if (style == 'list_rate')
+            return '<li><a href="/?v=' + id + '">'+rate+' &bull; ' + title + '</a></li>';
+    }
+
+
+
+
+
