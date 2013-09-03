2fa066f9f4dfc6d9cc57686bb7b6b1a4890b651e
Giacomo Zinetti <giacomo.zinetti@giko.it>
2013-09-03 12:28:18 +0200

    added config to gitignore

--- docroot/inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $silex['debug'] = true;
-
-    define('DB_HOST',           'localhost');
-    define('DB_USER',           'root');
-    define('DB_PASSWORD',       '');
-    define('DB_NAME',           'youtchouk');
-
-    $context['STATIC_URL'] = '/resources/';
